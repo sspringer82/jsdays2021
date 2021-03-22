caecf2b7f2bc9b39a4cd4ed29353371d1f22402c
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-03-22 16:07:09 +0100

    dom and contact

--- js4devs/src/index.js
+++ js4devs/src/index.js
@@ -10,4 +10,17 @@ console.log(str);
 document.addEventListener('DOMContentLoaded', function () {
   const element = document.querySelector('button');
   console.log(element);
+
+  const div = document.createElement('div');
+  div.innerText = 'Hallo Welt';
+  document.body.appendChild(div);
+
+  const names = ['Peter', 'Paul', 'Mary'];
+  const container = document.createElement('div');
+  names.forEach((name) => {
+    const div = document.createElement('div');
+    div.innerText = name;
+    container.appendChild(div);
+  });
+  document.body.appendChild(container);
 });
