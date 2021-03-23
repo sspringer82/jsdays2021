561365e83b59b880f88685d0ffebbde5120277ac
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-03-23 10:25:35 +0100

    destructured require

--- node/index.js
+++ node/index.js
@@ -1,6 +1,13 @@
-const readline = require('readline');
+// const readline = require('readline');
 
-const rl = readline.createInterface({
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+const { createInterface } = require('readline');
+
+const rl = createInterface({
   input: process.stdin,
   output: process.stdout,
 });
