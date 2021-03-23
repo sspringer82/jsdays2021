14ba0ac53736b6582893fd68bf6a8d729181d8fd
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-03-23 14:56:44 +0100

    web api

--- node/web/index.mjs
+++ node/web/index.mjs
@@ -2,9 +2,23 @@ import { createServer } from 'http';
 import { config } from 'dotenv';
 config();
 
-createServer((req, res) => res.end('Hallo Welt')).listen(
-  process.env.PORT,
-  () => {
-    console.log(`Server is listening to http://localhost:${process.env.PORT}`);
-  },
-);
+createServer((req, res) => {
+  const user = {
+    id: 1,
+    firstname: 'Klaus',
+    lastname: 'Müller',
+  };
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  if (req.url.startsWith('/user/')) {
+    if (req.method === 'GET') {
+      res.write(JSON.stringify(user));
+    }
+  } else if (req.url === '/user') {
+    if (req.method === 'GET') {
+      res.write(JSON.stringify([user, user]));
+    }
+  }
+  res.end();
+}).listen(process.env.PORT, () => {
+  console.log(`Server is listening to http://localhost:${process.env.PORT}`);
+});
