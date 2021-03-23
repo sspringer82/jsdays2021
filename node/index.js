d0ff0264ff325c18b779e60683f5ce1cef4c67e1
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-03-23 11:09:36 +0100

    tasks

--- node/index.js
+++ node/index.js
@@ -2,6 +2,12 @@ const Questionaire = require('./questionaire');
 
 const q = new Questionaire();
 
+const tasks = [
+  {o1: 1, o2: 2, r: 3}
+  {o1: 2, o2: 2, r: 4}
+  {o1: 4, o2: 4, r: 8}
+];
+
 q.question('a')
   .then((answer) => {
     console.log('Answer: ', answer);
@@ -12,7 +18,7 @@ q.question('a')
     console.log('Answer: ', answer);
 
     return q.question('c');
-  })
+  )
   .then((answer) => {
     console.log('Answer: ', answer);
 
