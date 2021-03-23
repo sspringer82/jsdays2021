c7e534ea1385bd73037e65968b87290470795298
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-03-23 10:21:53 +0100

    readline

--- node/index.js
+++ node/index.js
@@ -1,3 +1,13 @@
-const zweiundvierzig = require('./math');
+const readline = require('readline');
 
-console.log(zweiundvierzig());
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('What do you think of Node.js? ', (answer) => {
+  // TODO: Log the answer in a database
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+
+  rl.close();
+});
--- node/index.mjs
+++ node/index.mjs
@@ -1,6 +1,20 @@
-import { zweiundvierzig } from './math.mjs';
+// const readline = require('readline');
 
-console.log(zweiundvierzig());
+// import readline from 'readline';
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 
-console.log(process);
-console.log(process.argv);
+import { createInterface } from 'readline';
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('What do you think of Node.js? ', (answer) => {
+  // TODO: Log the answer in a database
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+
+  rl.close();
+});
