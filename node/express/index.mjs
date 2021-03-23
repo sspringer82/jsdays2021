f0715d2c421b7e5558e3f047fdbbf0b7c50587c5
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-03-23 15:34:29 +0100

    express

--- node/express/index.mjs
+++ node/express/index.mjs
@@ -8,9 +8,18 @@ const user = {
   lastname: 'Müller',
 };
 
-app.get('/', (req, res) => {
+app.use((req, res, next) => {
+  console.log(req);
+  next();
+});
+
+app.get('/:id', (req, res) => {
+  console.log(req.params.id);
   res.json(user);
 });
+// app.post();
+// app.put();
+// app.delete();
 
 app.listen(8081, () =>
   console.log('server is listening to http://localhost:8081'),
