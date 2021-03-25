4840806ec6d0267797e8d94055e905317510ab84
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-03-25 11:20:21 +0100

    fix new button

--- html5/index.js
+++ html5/index.js
@@ -4,22 +4,26 @@ import './form.js';
 document.addEventListener('DOMContentLoaded', () => {
   const pwList = document.querySelector('pw-list');
   const pwForm = document.querySelector('pw-form');
+  const newButton = document.querySelector('#new');
 
   pwList.addEventListener('edit', (e) => {
     pwForm.setAttribute('id', e.detail.id);
     pwList.style.display = 'none';
+    newButton.style.display = 'none';
     pwForm.style.display = 'block';
   });
 
   pwForm.addEventListener('save', (e) => {
     pwList.setAttribute('update', Date.now());
     pwList.style.display = 'block';
+    newButton.style.display = 'block';
     pwForm.style.display = 'none';
   });
 
   document.getElementById('new').addEventListener('click', () => {
     pwForm.setAttribute('id', '');
     pwList.style.display = 'none';
+    newButton.style.display = 'none';
     pwForm.style.display = 'block';
   });
 });
