f9cdee58738ebc5b6cb32f5356c0da0f80375f1a
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-03-25 15:36:59 +0100

    crypto

--- html5/model.js
+++ html5/model.js
@@ -1,4 +1,6 @@
-const db = new Dexie('pw');
+import { encrypt, decrypt } from './crypto.js';
+
+export const db = new Dexie('pw');
 db.version(1).stores({
   pw: '++id, username, password, url',
   secret: '++id',
@@ -12,21 +14,31 @@ const model = {
       this.addElement(entry);
     }
   },
-  addElement(entry) {
+  async addElement(entry) {
     delete entry.id;
+    entry.password = await encrypt(entry.password);
     return db.pw.add(entry);
   },
-  editElement(entry) {
+  async editElement(entry) {
+    entry.password = await encrypt(entry.password);
     return db.pw.update(entry.id, entry);
   },
   deleteElement(id) {
     return db.pw.delete(id);
   },
-  getAllElements() {
-    return db.pw.toArray();
+  async getAllElements() {
+    const data = await db.pw.toArray();
+    return Promise.all(
+      data.map(async (entry) => ({
+        ...entry,
+        password: await decrypt(entry.password),
+      })),
+    );
   },
-  getOneById(id) {
-    return db.pw.get(id);
+  async getOneById(id) {
+    const data = await db.pw.get(id);
+    data.password = await decript(data.password);
+    return data;
   },
 };
 
