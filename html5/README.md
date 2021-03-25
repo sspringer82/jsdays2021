6ba0228f40fd0dc9fa2a875960eabbc0e0e70352
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-03-25 09:26:09 +0100

    use a web server

---
+++ html5/README.md
@@ -0,0 +1,5 @@
+# Links
+
+- Secure Context: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts
+- Features im Secure Context: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts/features_restricted_to_secure_contexts
+- https://caniuse.com/
--- html5/index.js
+++ html5/index.js
@@ -1,84 +1,83 @@
 let data = [
   {
     id: 1,
-    username: "test",
-    password: "p@55w0rD",
-    url: "https://google.com",
+    username: 'test',
+    password: 'p@55w0rD',
+    url: 'https://google.com',
     notes: "Don't give your password away",
   },
   {
     id: 2,
-    username: "test1",
-    password: "p@55w0rD",
-    url: "https://live.com",
+    username: 'test1',
+    password: 'p@55w0rD',
+    url: 'https://live.com',
     notes: "Don't give your password away",
   },
 ];
 
-document.addEventListener("DOMContentLoaded", () => {
-  
+document.addEventListener('DOMContentLoaded', () => {
   buildTable();
 
-  document.getElementById("new").addEventListener("click", createEntry);
-  document.getElementById("form").addEventListener("submit", save);
-  document.getElementById("cancel").addEventListener("click", cancel);
+  document.getElementById('new').addEventListener('click', createEntry);
+  document.getElementById('form').addEventListener('submit', save);
+  document.getElementById('cancel').addEventListener('click', cancel);
 });
 
 function buildTable() {
-  const tableContainer = document.getElementById("list");
-  tableContainer.innerHTML = "";
+  const tableContainer = document.getElementById('list');
+  tableContainer.innerHTML = '';
 
-  const table = document.createElement("table");
+  const table = document.createElement('table');
   data.forEach((rowData) => createRow(rowData, table));
   tableContainer.appendChild(table);
 
-  table.addEventListener("click", (e) => {
-    if (e.target.classList.contains("delete")) {
+  table.addEventListener('click', (e) => {
+    if (e.target.classList.contains('delete')) {
       deleteEntry(e.target.dataset.id);
-    } else if (e.target.classList.contains("edit")) {
+    } else if (e.target.classList.contains('edit')) {
       editEntry(
-        data.find((entry) => entry.id === parseInt(e.target.dataset.id, 10))
+        data.find((entry) => entry.id === parseInt(e.target.dataset.id, 10)),
       );
     }
   });
 }
 
 function createRow(data, table) {
-  const tr = document.createElement("tr");
+  const tr = document.createElement('tr');
 
   // url
-  const urlTd = document.createElement("td");
+  const urlTd = document.createElement('td');
   urlTd.innerText = data.url;
   tr.appendChild(urlTd);
 
   // username
-  const usernameTd = document.createElement("td");
+  const usernameTd = document.createElement('td');
   usernameTd.innerText = data.username;
   tr.appendChild(usernameTd);
 
   // password
-  const passwordTd = document.createElement("td");
-  passwordTd.innerText = "*".repeat(data.password.length);
+  const passwordTd = document.createElement('td');
+  passwordTd.innerText = '*'.repeat(data.password.length);
   tr.appendChild(passwordTd);
 
   // delete button
-  const deleteButton = document.createElement("button");
-  deleteButton.classList.add("delete");
+  const deleteButton = document.createElement('button');
+  deleteButton.classList.add('delete');
   deleteButton.dataset.id = data.id;
-  deleteButton.innerText = "delete";
+  deleteButton.innerText = 'delete';
   tr.appendChild(deleteButton);
 
-  const editButton = document.createElement("button");
-  editButton.classList.add("edit");
+  const editButton = document.createElement('button');
+  editButton.classList.add('edit');
   editButton.dataset.id = data.id;
-  editButton.innerText = "edit";
+  editButton.innerText = 'edit';
   tr.appendChild(editButton);
 
   table.appendChild(tr);
 }
 
 function deleteEntry(id) {
-  const tableContainer = document.getElementById("list");
+  const tableContainer = document.getElementById('list');
   data = data.filter((row) => row.id !== parseInt(id, 10));
   buildTable(data, tableContainer);
 }
@@ -97,20 +96,20 @@ function createEntry() {
 }
 
 function showAndResetForm() {
-  document.getElementById("list").style.display = "none";
-  document.getElementById("new").style.display = "none";
+  document.getElementById('list').style.display = 'none';
+  document.getElementById('new').style.display = 'none';
 
-  const form = document.getElementById("form");
-  form.style.display = "block";
+  const form = document.getElementById('form');
+  form.style.display = 'block';
   form.reset();
 }
 
 function showAndRefreshTable() {
-  const tableContainer = document.getElementById("list");
+  const tableContainer = document.getElementById('list');
   buildTable(data, tableContainer);
-  tableContainer.style.display = "block";
-  document.getElementById("new").style.display = "block";
-  document.getElementById("form").style.display = "none";
+  tableContainer.style.display = 'block';
+  document.getElementById('new').style.display = 'block';
+  document.getElementById('form').style.display = 'none';
 }
 
 function cancel() {
@@ -126,13 +125,13 @@ function save(e) {
     password: document.getElementById('password').value,
     url: document.getElementById('url').value,
     notes: document.getElementById('notes').value,
-  }
+  };
 
   if (entry.id) {
-    const id = data.findIndex(e => e.id === entry.id);
+    const id = data.findIndex((e) => e.id === entry.id);
     data[id] = entry;
   } else {
-    entry.id = Math.max(...data.map(e => e.id)) +1;
+    entry.id = Math.max(...data.map((e) => e.id)) + 1;
     data.push(entry);
   }
 
