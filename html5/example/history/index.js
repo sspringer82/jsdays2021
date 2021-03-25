5119db7bab754f40d2249891d8609719b44ccf1a
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-03-25 11:51:47 +0100

    additional info

--- html5/example/history/index.js
+++ html5/example/history/index.js
@@ -1,10 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
   const button = document.querySelector('button');
-  button.addEventListener('click', () => {
-    history.pushState('data', 'title', 'url');
+  button.addEventListener('click', (e) => {
+    const id = e.target.id;
+    history.pushState({ id }, 'title', `/edit/${id}`);
   });
 });
 
 window.onpopstate = (e) => {
-  console.log(e);
+  if (e.state?.id) {
+    console.log('fetching data with id: ', e.state.id);
+  }
 };
